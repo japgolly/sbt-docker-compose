fbb1432357ff7035d9112ca6d0fdbe0f6f57bc3b
David Barri <japgolly@gmail.com>
2026-01-28 10:42:43 +1100

    Refactor

--- src/main/scala/DockerComposeEnv.scala
+++ src/main/scala/DockerComposeEnv.scala
@@ -5,36 +5,6 @@ import sbt._
 object DockerComposeEnv {
   import sys.process._
 
-  def javaOptionsFromDockerComposeEnv(serviceName: String, envRoot: File, filename: String = "docker-compose.yml"): List[String] = {
-    val service = s"  $serviceName:"
-    val envVar = "\\$\\{?([A-Za-z0-9_]+)\\}?".r
-    def processEntry(e: String) =
-      envVar.replaceAllIn(e, m => envFileValue(envRoot, m group 1))
-    var inService = false
-    var inEnv = false
-    val b = List.newBuilder[String]
-    IO.readLines(envRoot / filename) foreach {
-      case `service`                                           => inService = true
-      case s if s.matches("^  [a-z].*:")                       => inService = false; inEnv = false
-      case "    environment:"                                  => inEnv = true
-      case s if s.matches("^    [a-z].*:")                     => inEnv = false
-      case s if inService && inEnv && s.startsWith("      - ") => b += "-D" + processEntry(s.drop(8).trim)
-      case _                                                   => ()
-    }
-    b.result()
-  }
-
-  def javaOptionsFromProps(props: File): List[String] =
-    javaOptionsFromProps(IO readLines props)
-
-  def javaOptionsFromProps(props: List[String]): List[String] =
-    props
-      .iterator
-      .map(_.replaceFirst(" *#.+", "").trim.replaceFirst(" *= *", "="))
-      .filter(_.nonEmpty)
-      .map("-D" + _)
-      .toList
-
   def envFileValue(envRoot: File, key: String): String = {
     val k = key + "="
     val f = envRoot / ".env"
@@ -45,13 +15,47 @@ object DockerComposeEnv {
   }
 
   final case class JavaOptions(asList: List[String]) extends AnyVal {
+
     def add(k: String, v: String): JavaOptions =
       JavaOptions(s"-D$k=$v" :: asList.filterNot(_ startsWith s"-D$k="))
+
+    def ++(other: JavaOptions): JavaOptions =
+      JavaOptions(asList ++ other.asList)
   }
 
   object JavaOptions {
-    def fromDockerComposeEnv(serviceName: String, envRoot: File, filename: String = "docker-compose.yml") =
-      apply(DockerComposeEnv.javaOptionsFromDockerComposeEnv(serviceName, envRoot, filename))
+
+    def fromDockerCompose(serviceName: String, envRoot: File, filename: String = "docker-compose.yml"): JavaOptions = {
+      val service = s"  $serviceName:"
+      val envVar = "\\$\\{?([A-Za-z0-9_]+)\\}?".r
+      def processEntry(e: String) =
+        envVar.replaceAllIn(e, m => envFileValue(envRoot, m group 1))
+      var inService = false
+      var inEnv = false
+      val b = List.newBuilder[String]
+      IO.readLines(envRoot / filename) foreach {
+        case `service`                                           => inService = true
+        case s if s.matches("^  [a-z].*:")                       => inService = false; inEnv = false
+        case "    environment:"                                  => inEnv = true
+        case s if s.matches("^    [a-z].*:")                     => inEnv = false
+        case s if inService && inEnv && s.startsWith("      - ") => b += "-D" + processEntry(s.drop(8).trim)
+        case _                                                   => ()
+      }
+      apply(b.result())
+    }
+
+    def fromProps(props: File): JavaOptions =
+      fromProps(IO readLines props)
+
+    def fromProps(propsLines: List[String]): JavaOptions = {
+      val list = propsLines
+        .iterator
+        .map(_.replaceFirst(" *#.+", "").trim.replaceFirst(" *= *", "="))
+        .filter(_.nonEmpty)
+        .map("-D" + _)
+        .toList
+      apply(list)
+    }
   }
 
   final class Services(startFn: () => Unit, stopFn: () => Unit) {
